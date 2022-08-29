6610a2ce5ce68d9a40bdb723de0c1f85de2e0595
Leonid <90571705+k1metsu@users.noreply.github.com>
2022-08-29 16:00:33 +0300

    Created 4 Activities, transitions to other activities implemented

---
+++ app/src/main/java/otus/gpb/homework/activities/ActivityA.kt
@@ -0,0 +1,16 @@
+package otus.gpb.homework.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ActivityA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a)
+        findViewById<Button>(R.id.action_a_btn).setOnClickListener {
+            startActivity(Intent(this, ActivityB::class.java))
+        }
+    }
+}
