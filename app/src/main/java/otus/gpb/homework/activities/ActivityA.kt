ca05071068d049d7c3afb5975b1029998d1654a5
coolkosta <kostia.borzakov@gmail.com>
2022-08-31 00:59:15 +0300

    Решение первого ДЗ

---
+++ app/src/main/java/otus/gpb/homework/activities/ActivityA.kt
@@ -0,0 +1,21 @@
+package otus.gpb.homework.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityA : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity)
+
+        findViewById<Button>(R.id.open_b_button).setOnClickListener {
+            val intent = Intent(this, ActivityB::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+
+        }
+    }
+}
