55fc25cfc4c3a5e38822f7fb45d4363c73a21e72
Zanozaroom <etn88@yandex.ru>
2022-08-31 10:51:18 +0700

    Created Activities A-D and add layouts for them

---
+++ app/src/main/java/otus/gpb/homework/activities/ActivityA.kt
@@ -0,0 +1,14 @@
+package otus.gpb.homework.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ActivityA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity_a)
+    Log.i("TASK", "Id: $taskId ActivityA_onCreate $this")
+
+    }
+}
