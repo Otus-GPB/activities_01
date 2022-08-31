bcde54fdd8b30c7171fbf8af40dbcc73c2a7b1e5
Vojan-Najov <serejah.mashichev@gmail.com>
2022-08-31 16:00:45 +0300

    add implicit intent for send email

--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -1,10 +1,12 @@
 package otus.gpb.homework.activities.sender
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class SenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +18,30 @@ class SenderActivity : AppCompatActivity() {
             val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
                 .setPackage("com.google.android.apps.maps")
                 .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(mapIntent)
+            try {
+                startActivity(mapIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast
+                    .makeText(this, "Google Maps not found.", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
         findViewById<Button>(R.id.buttonSendEmail).setOnClickListener {
+            val emailRecipient = "android@otus.ru"
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:$emailRecipient")
+                putExtra(Intent.EXTRA_EMAIL, emailRecipient)
+                putExtra(Intent.EXTRA_SUBJECT, "Homework Activities_02")
+                putExtra(Intent.EXTRA_TEXT, "Hello, Otus!")
+            }
+            try {
+                startActivity(Intent.createChooser(sendIntent, "Choose email client:"))
+            } catch (e: ActivityNotFoundException) {
+                Toast
+                    .makeText(this, "You can't send an email", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
         findViewById<Button>(R.id.buttonOpenReceiver).setOnClickListener {
         }
