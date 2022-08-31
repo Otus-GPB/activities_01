e072fb0ea1436806bc26eeff3860e216ac8b3405
Vojan-Najov <serejah.mashichev@gmail.com>
2022-08-31 13:44:52 +0300

    add explicit intent for button 'To Google Map'

--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -1,5 +1,7 @@
 package otus.gpb.homework.activities.sender
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,6 +12,11 @@ class SenderActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sender)
 
         findViewById<Button>(R.id.buttonToGoogleMaps).setOnClickListener {
+            val gmmIntentUri = Uri.parse("geo:0,0?q=restaurants")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                .setPackage("com.google.android.apps.maps")
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(mapIntent)
         }
         findViewById<Button>(R.id.buttonSendEmail).setOnClickListener {
         }
