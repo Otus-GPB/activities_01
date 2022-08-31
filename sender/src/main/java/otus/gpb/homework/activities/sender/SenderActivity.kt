db9661d8bd9b2f791aa9bce07fcefe1a5ce608b4
Vojan-Najov <serejah.mashichev@gmail.com>
2022-08-31 16:32:26 +0300

    add chooseRestaurant and sendEmail functions

--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -13,37 +13,37 @@ class SenderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sender)
 
-        findViewById<Button>(R.id.buttonToGoogleMaps).setOnClickListener {
-            val gmmIntentUri = Uri.parse("geo:0,0?q=restaurants")
-            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-                .setPackage("com.google.android.apps.maps")
-                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            try {
-                startActivity(mapIntent)
-            } catch (e: ActivityNotFoundException) {
-                Toast
-                    .makeText(this, "Google Maps not found.", Toast.LENGTH_SHORT)
-                    .show()
-            }
+        findViewById<Button>(R.id.buttonToGoogleMaps).setOnClickListener { chooseRestaurant() }
+        findViewById<Button>(R.id.buttonSendEmail).setOnClickListener { sendEmail()}
+        findViewById<Button>(R.id.buttonOpenReceiver).setOnClickListener { }
+    }
+
+    private fun chooseRestaurant() {
+        val gmmIntentUri = Uri.parse("geo:0,0?q=restaurants")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            .setPackage("com.google.android.apps.maps")
+        try {
+            startActivity(mapIntent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Google Maps not found.", Toast.LENGTH_SHORT).show()
         }
-        findViewById<Button>(R.id.buttonSendEmail).setOnClickListener {
-            val emailRecipient = "android@otus.ru"
-            val sendIntent = Intent().apply {
-                action = Intent.ACTION_SENDTO
-                data = Uri.parse("mailto:$emailRecipient")
-                putExtra(Intent.EXTRA_EMAIL, emailRecipient)
-                putExtra(Intent.EXTRA_SUBJECT, "Homework Activities_02")
-                putExtra(Intent.EXTRA_TEXT, "Hello, Otus!")
-            }
-            try {
-                startActivity(Intent.createChooser(sendIntent, "Choose email client:"))
-            } catch (e: ActivityNotFoundException) {
-                Toast
-                    .makeText(this, "You can't send an email", Toast.LENGTH_SHORT)
-                    .show()
-            }
+    }
+    private fun sendEmail() {
+        val emailRecipient = resources.getString(R.string.email_recipient)
+        val emailSubject = resources.getString(R.string.email_subject)
+        val emailText = resources.getString(R.string.email_text)
+        val chooserTitle = resources.getString(R.string.chooser_title)
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SENDTO
+            data = Uri.parse("mailto:$emailRecipient")
+            putExtra(Intent.EXTRA_EMAIL, emailRecipient)
+            putExtra(Intent.EXTRA_SUBJECT, emailSubject)
+            putExtra(Intent.EXTRA_TEXT, emailText)
         }
-        findViewById<Button>(R.id.buttonOpenReceiver).setOnClickListener {
+        try {
+            startActivity(Intent.createChooser(sendIntent, chooserTitle))
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "You can't send an email.", Toast.LENGTH_SHORT).show()
         }
     }
 }
