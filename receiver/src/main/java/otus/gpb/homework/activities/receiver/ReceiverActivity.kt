0e62aef189411162717da52ea913528401527e0b
alexander <alexandersznov@gmail.com>
2022-09-06 01:08:44 +0300

    Выполнил 3 задание

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,6 +1,12 @@
 package otus.gpb.homework.activities.receiver
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReceiverActivity : AppCompatActivity() {
@@ -8,5 +14,23 @@ class ReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+        val posterImageView = findViewById<ImageView>(R.id.posterImageView)
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val descriptionTextView = findViewById<TextView>(R.id.descriptionTextView)
+        val yearTextView = findViewById<TextView>(R.id.yearTextView)
+        if (intent?.action == Intent.ACTION_SEND && intent?.type == "text/plain"){
+            val title = intent.getStringExtra("title")
+            val year = intent.getStringExtra("year")
+            val description = intent.getStringExtra("description")
+            val image = if (title == "Интерстеллар"){
+                getDrawable(R.drawable.interstellar)
+            } else{
+                getDrawable(R.drawable.niceguys)
+            }
+            posterImageView.setImageDrawable(image)
+            titleTextView.text = title
+            descriptionTextView.text = description
+            yearTextView.text = year.toString()
+        }
     }
 }
--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -4,9 +4,38 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
+const val TAG = "SenderActivity"
 
 class SenderActivity : AppCompatActivity() {
+    val films = listOf(
+        Payload(
+            "Интерстеллар",
+            "2014",
+            "Когда засуха, пыльные бури и вымирание растений приводят человечество к " +
+                    "продовольственному кризису, коллектив исследователей и учёных отправляется " +
+                    "сквозь червоточину (которая предположительно соединяет области " +
+                    "пространства-времени через большое расстояние) в путешествие, чтобы превзойти " +
+                    "прежние ограничения для космических путешествий человека и найти планету с " +
+                    "подходящими для человечества условиями."
+        ),
+        Payload(
+            "Плохие парни",
+            "2016",
+            "Что бывает, когда напарником брутального костолома становится субтильный " +
+                    "лопух? Наемный охранник Джексон Хили и частный детектив Холланд Марч вынуждены " +
+                    "работать в паре, чтобы распутать плевое дело о пропавшей девушке, которое " +
+                    "оборачивается преступлением века. Смогут ли парни разгадать сложный ребус, " +
+                    "если у каждого из них – свои, весьма индивидуальные методы."
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sender)
@@ -29,6 +58,19 @@ class SenderActivity : AppCompatActivity() {
             startActivity(intent)
         }
         val btnOpenReceiver = findViewById<Button>(R.id.btn_open_receiver)
-        btnOpenReceiver.setOnClickListener {  }
+        btnOpenReceiver.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                addCategory(Intent.CATEGORY_DEFAULT)
+                val film = films.random()
+                putExtra("title", film.title)
+                putExtra("year", film.year)
+                putExtra("description", film.description)
+            }
+            startActivity(intent)
+
+        }
+
     }
+
 }
