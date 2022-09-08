866c12f3d461fea77258ce6aff2990065ff250f6
Elena Spytsyna <lspoke.larn@gmail.com>
2022-09-08 17:08:05 +0300

    color and listener
    add color and empty onClickListener for activities

--- app/src/main/java/otus/gpb/homework/activities/ActivityA.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityA.kt
@@ -2,10 +2,14 @@ package otus.gpb.homework.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ActivityA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a)
+        findViewById<Button>(R.id.btnOpenB).setOnClickListener {
+
+        }
     }
 }
--- app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
@@ -2,10 +2,23 @@ package otus.gpb.homework.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ActivityC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_c)
+        findViewById<Button>(R.id.btnOpenA).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.btnOpenD).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.btnCloseC).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.btnCloseStack).setOnClickListener {
+
+        }
     }
 }
