58f9d9fa4425d9f444e96b56c747fc55dc9afa14
lanalebedeva <batetnikova@mail.ru>
2022-09-09 16:34:12 +0300

    add: task1 in Activity #3

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -1,14 +1,24 @@
 package otus.gpb.homework.activities
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+
+private const val TAG = "EditProfileActivity"
+private const val PERMISSION_REQUEST_CAMERA = 0
 
 class EditProfileActivity : AppCompatActivity() {
-
     private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +38,100 @@ class EditProfileActivity : AppCompatActivity() {
                 }
             }
         }
+        findViewById<ImageView>(R.id.imageview_photo).setOnClickListener { takePhoto() }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.isEmpty()) {
+                Log.d(TAG, "PERMISSION_REQUEST_CAMERA do not have")
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "есть разрешение")
+                startCamera()
+            } else {
+                Log.d(TAG, "ничего не делать!")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun takePhoto() {
+        val items = arrayOf("Сделать фото", "Выбрать фото")
+
+        MaterialAlertDialogBuilder(this)
+            .setItems(items) { _, which ->
+                when (which) {
+                    0 -> showCameraPreview()
+                    1 -> Log.d(TAG, "Выбрать фото")
+                }
+            }.show()
+    }
+
+    private fun showCameraPreview() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            startCamera()
+        } else {
+            doCameraPermission()
+        }
+    }
+
+    private fun doCameraPermission() {
+        Log.d(TAG, "Requested camera permission")
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            showRationaleDialog()
+        } else {
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            PERMISSION_REQUEST_CAMERA
+        )
+    }
+
+    private fun showRationaleDialog() {
+        Log.d(TAG, "Show rational dialog")
+        val items = arrayOf("Дать доступ", "Отмена")
+
+        MaterialAlertDialogBuilder(this)
+            .setItems(items) { _, which ->
+                when (which) {
+                    0 -> requestCameraPermission()
+                    1 -> showOpenSettingsDialog()
+                }
+            }.show()
+    }
+
+    private fun showOpenSettingsDialog() {
+        val items = arrayOf("Открыть настройки")
+
+        MaterialAlertDialogBuilder(this)
+            .setItems(items) { _, which ->
+                when (which) {
+                    0 -> openAppSettings()
+                }
+            }.show()
+    }
+
+    private fun openAppSettings() {
+        val intent = Intent(android.provider.Settings.ACTION_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun startCamera() {
+        Log.d(TAG, "Start Camera")
+        imageView.setImageResource(R.drawable.cat)
     }
 
     /**
