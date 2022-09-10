de9692264d35e143299b9b2ff8b0893f0810f1ca
by_westik <nastya.derevyanko.2020@mail.ru>
2022-09-10 21:26:58 +0300

    Выполнение пункта 2: с помощью неявного интента отправить письмо по почте

--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -2,9 +2,9 @@ package otus.gpb.homework.activities.sender
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class SenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +17,19 @@ class SenderActivity : AppCompatActivity() {
             val mapIntent = Intent(Intent.ACTION_VIEW, uriRestaurant)
             mapIntent.setPackage("com.google.android.apps.maps")
             startActivity(mapIntent)
+        }
+
+        val btnSendMail = findViewById<Button>(R.id.button_send_Email)
+        val mail = "android@otus.ru"
+        btnSendMail.setOnClickListener {
+            val sendIntent = Intent(Intent.ACTION_SEND)
+            sendIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(mail)) //Почему, если написать putExtra(Intent.EXTRA_EMAIL, "android@otus.ru")
+                                            // или putExtra(Intent.EXTRA_EMAIL, mail), то почта не добавляется в получателя?
+            sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Simple Subject")
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Simple text from sender app!")
+            sendIntent.type = "message/rfc822"
+
+            startActivity(Intent.createChooser(sendIntent, "Send mail using..."));
         }
     }
 }
