ea5c4adbf21806ebc95c11b69623e2f5e5ba7f4e
by_westik <nastya.derevyanko.2020@mail.ru>
2022-09-13 00:56:02 +0300

    Выполнение пункта 3

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,12 +1,24 @@
 package otus.gpb.homework.activities.receiver
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class ReceiverActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        findViewById<TextView>(R.id.titleTextView).setText(intent.getStringExtra("title"))
+        findViewById<TextView>(R.id.yearTextView).setText(intent.getStringExtra("year"))
+        findViewById<TextView>(R.id.descriptionTextView).setText(intent.getStringExtra("description"))
+
+        val poster = ContextCompat.getDrawable(this, R.drawable.niceguys)
+        findViewById<ImageView>(R.id.posterImageView).setImageDrawable(poster)
     }
 }
--- sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
+++ sender/src/main/java/otus/gpb/homework/activities/sender/SenderActivity.kt
@@ -1,12 +1,20 @@
 package otus.gpb.homework.activities.sender
 
+import android.content.ClipDescription
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.io.InputStream
+
+private const val EXTRA_TITLE = "title"
+private const val EXTRA_YEAR = "year"
+private const val EXTRA_DESCRIPTION = "description"
 
 class SenderActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sender)
@@ -20,16 +28,28 @@ class SenderActivity : AppCompatActivity() {
         }
 
         val btnSendMail = findViewById<Button>(R.id.button_send_Email)
-        val mail = "android@otus.ru"
         btnSendMail.setOnClickListener {
             val sendIntent = Intent(Intent.ACTION_SEND)
-            sendIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(mail)) //Почему, если написать putExtra(Intent.EXTRA_EMAIL, "android@otus.ru")
-                                            // или putExtra(Intent.EXTRA_EMAIL, mail), то почта не добавляется в получателя?
-            sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Simple Subject")
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "Simple text from sender app!")
+            sendIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(resources.getString(R.string.email))) //Почему, если написать putExtra(Intent.EXTRA_EMAIL, "android@otus.ru")
+                                            // или putExtra(Intent.EXTRA_EMAIL, resources.getString(R.string.email)), то почта не добавляется в получателя?
+            sendIntent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.subject))
+            sendIntent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.text))
             sendIntent.type = "message/rfc822"
 
-            startActivity(Intent.createChooser(sendIntent, "Send mail using..."));
+            startActivity(sendIntent)
         }
+
+        val btnToReceiver = findViewById<Button>(R.id.button_open_Reveiver)
+        btnToReceiver.setOnClickListener {
+            val openIntent = Intent(Intent.ACTION_SEND)
+            openIntent.type = "text/plain"
+            openIntent.addCategory(Intent.CATEGORY_DEFAULT)
+            openIntent.putExtra(EXTRA_TITLE, resources.getString(R.string.film_title))
+            openIntent.putExtra(EXTRA_YEAR, resources.getString(R.string.film_year))
+            openIntent.putExtra(EXTRA_DESCRIPTION, resources.getString(R.string.film_description))
+
+            startActivity(openIntent)
+        }
+
     }
 }
