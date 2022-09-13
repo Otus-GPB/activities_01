bde109798461e0591e57f483c29f14d0152fa73e
coolkosta <kostia.borzakov@gmail.com>
2022-09-13 18:18:22 +0300

    Реализация ДЗ №3 задача №1

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -1,15 +1,33 @@
 package otus.gpb.homework.activities
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
+
+    private val launcherPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted: Boolean ->
+            if (granted) {
+                getPicture()
+            } else if (!shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                openSettingsDialog()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +46,82 @@ class EditProfileActivity : AppCompatActivity() {
                 }
             }
         }
+
+        imageView.setOnClickListener {
+
+            val items = arrayOf(getString(R.string.make_photo), getString(R.string.choose_photo))
+
+            MaterialAlertDialogBuilder(this)
+                .setTitle(getString(R.string.choose_method))
+                .setItems(items) { dialog, which ->
+                    when (which) {
+                        0 -> checkPermissionWithRationale()
+                    }
+                }
+                .show()
+        }
     }
+
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun getPicture() {
+        findViewById<ImageView>(R.id.imageview_photo).setImageDrawable(getDrawable(R.drawable.cat))
+    }
+
+
+    private fun checkPermissionWithRationale() {
+
+        val permissionCheck = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        )
+
+        val requestRationale: Boolean = shouldShowRequestPermissionRationale(
+            Manifest.permission.CAMERA
+        )
+
+        when {
+            permissionCheck == PackageManager.PERMISSION_GRANTED -> getPicture()
+            requestRationale -> showRationalDialog()
+
+            else -> launcherPermission.launch(
+                Manifest.permission.CAMERA
+            )
+
+        }
+
+
+    }
+
+
+    private fun showRationalDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.get_permission))
+            .setMessage(getString(R.string.get_access_to_camera))
+            .setNegativeButton(getString(R.string.denie_access)) { dialog, which ->
+                dialog.cancel()
+            }
+            .setPositiveButton(getString(R.string.yes)) { dialog, which ->
+                launcherPermission.launch(Manifest.permission.CAMERA)
+
+            }
+            .show()
+    }
+
+    private fun openSettingsDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.open_settings))
+            .setMessage(getString(R.string.open_settings_for_permissions))
+            .setPositiveButton(getString(R.string.good)) { dialog, which ->
+                val intent = Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:$packageName")
+                )
+                startActivity(intent)
+            }
+            .show()
+    }
+
 
     /**
      * Используйте этот метод чтобы отобразить картинку полученную из медиатеки в ImageView
