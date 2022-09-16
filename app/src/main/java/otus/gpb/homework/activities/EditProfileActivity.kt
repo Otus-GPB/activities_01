ab72f3adb8d217e8581814f50ad584f5e6cc42fa
Anna <taasonei@gmail.com>
2022-09-16 22:18:53 +0300

    ДЗ 3. Задание 1. При клике на ImageView открывается диалог с "Сделать фото", "Выбрать фото". При клике на сделать фото проверяется доступ к камере. Если доступ есть - загружается фото с котом. Если доступа нет, то показывается стадартный диалог, при отмене - rationale. Если пользователь дважды отказался предоставить доступ, показывается диалог с кнопкой для перехода в настройки приложения

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -1,20 +1,40 @@
 package otus.gpb.homework.activities
 
+import android.Manifest.permission.CAMERA
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                setCatPhoto()
+            } else if (!shouldShowRequestPermissionRationale(CAMERA)) {
+                showOpenSettingsDialog()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         imageView = findViewById(R.id.imageview_photo)
+
+        imageView.setOnClickListener {
+            showPhotoActionDialog()
+        }
 
         findViewById<Toolbar>(R.id.toolbar).apply {
             inflateMenu(R.menu.menu)
@@ -30,12 +50,71 @@ class EditProfileActivity : AppCompatActivity() {
         }
     }
 
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> setCatPhoto()
+            shouldShowRequestPermissionRationale(CAMERA) -> showCameraPermissionDialog()
+            else -> requestPermissionLauncher.launch(CAMERA)
+        }
+    }
+
+    private fun showPhotoActionDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.select_action))
+            .setItems(
+                arrayOf(
+                    getString(R.string.make_photo),
+                    getString(R.string.select_photo)
+                )
+            ) { _, which ->
+                when (which) {
+                    0 -> requestCameraPermission()
+                    1 -> {}
+                }
+            }
+            .show()
+    }
+
+    private fun showCameraPermissionDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.require_camera_access))
+            .setMessage(getString(R.string.camera_permission_rational_message))
+            .setPositiveButton(getString(R.string.provide_access)) { _, _ ->
+                requestPermissionLauncher.launch(CAMERA)
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun showOpenSettingsDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.require_camera_access))
+            .setMessage(getString(R.string.camera_open_settings_message))
+            .setPositiveButton(getString(R.string.open_settings)) { _, _ ->
+                val intent = Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", packageName, null)
+                )
+                startActivity(intent)
+            }
+            .show()
+    }
+
     /**
      * Используйте этот метод чтобы отобразить картинку полученную из медиатеки в ImageView
      */
     private fun populateImage(uri: Uri) {
         val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
         imageView.setImageBitmap(bitmap)
+    }
+
+    private fun setCatPhoto() {
+        imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.cat))
     }
 
     private fun openSenderApp() {
