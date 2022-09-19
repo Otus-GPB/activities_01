358253f23c0d6406edcbee7d101256f01d27979a
lanalebedeva <batetnikova@mail.ru>
2022-09-19 14:16:51 +0300

    убрала зокоммментированный код

--- app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
@@ -18,7 +18,6 @@ class ActivityC : AppCompatActivity() {
         findViewById<Button>(R.id.button_open_ActivityA).setOnClickListener {
             Log.d(TAG, "press button open Activity A")
             val intent = Intent(this, ActivityA::class.java)
-            intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
         findViewById<Button>(R.id.button_open_ActivityD).setOnClickListener {
@@ -33,11 +32,7 @@ class ActivityC : AppCompatActivity() {
             finish()
         }
         findViewById<Button>(R.id.button_close_stack).setOnClickListener {
-            Log.d(TAG, "press button close stack")
-//            val intent = Intent(this, ActivityA::class.java)
-//            intent.addFlags(FLAG_ACTIVITY_CLEAR_TASK)
-//            intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
-//            startActivity(intent)
+            Log.d(TAG, "press button close stack")git
             finishAffinity()
         }
     }
