10988fb188c4350a76c5fe85ffdbd00d70682daf
Zanozaroom <etn88@yandex.ru>
2022-10-03 13:15:38 +0700

    Code was done adaptation by Detect rules

--- app/src/main/java/otus/gpb/homework/activities/FillFormActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/FillFormActivity.kt
@@ -1,76 +1,70 @@
 package otus.gpb.homework.activities
-
-
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
-import android.view.KeyEvent
-import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.parcelize.Parcelize
 
 class FillFormActivity : AppCompatActivity() {
-    lateinit var user_object: UserFormData
-    lateinit var user_form_result: Intent
-    lateinit var text_firstname: EditText
-    lateinit var text_secondname: EditText
-    lateinit var text_age: EditText
+    lateinit var userObject: UserFormData
+    lateinit var userFormresult: Intent
+    lateinit var textFirstname: EditText
+    lateinit var textSecondname: EditText
+    lateinit var textAge: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        user_object = savedInstanceState?.getParcelable<UserFormData>(KEY_OUTPUT_VALUE)
-            ?: intent.getParcelableExtra<UserFormData>(INPUT_VALUE)
-                    ?: UserFormData("", "", 0)
+        userObject = savedInstanceState?.getParcelable<UserFormData>(KEY_OUTPUT_VALUE)
+            ?: intent.getParcelableExtra<UserFormData>(INPUT_VALUE) ?: UserFormData("", "", 0)
         setContentView(R.layout.activity_fill_form)
-        text_firstname = findViewById(R.id.edit_firstname)
-        text_secondname = findViewById(R.id.edit_secondname)
-        text_age = findViewById(R.id.edit_age)
+        textFirstname = findViewById(R.id.edit_firstname)
+        textSecondname = findViewById(R.id.edit_secondname)
+        textAge = findViewById(R.id.edit_age)
 
-        if (user_object.realCreate == true) {
+        if (userObject.realCreate == true) {
             updateUI()
         }
-        text_age.setOnEditorActionListener { v, actionId, _ ->
+        textAge.setOnEditorActionListener { v, actionId, _ ->
             saveUser()
             true
         }
 
-        val b_save = findViewById<Button>(R.id.save_user)
-        b_save.setOnClickListener {
+        val bSave = findViewById<Button>(R.id.save_user)
+        bSave.setOnClickListener {
             saveUser()
         }
     }
 
     private fun updateUI() {
-        text_firstname.setHint("...редактировать: ${user_object.firstname.toString()}")
-        text_secondname.setHint("...редактировать: ${user_object.secondname.toString()}")
-        text_age.setHint("...редактировать: ${user_object.age.toString()}")
+        textFirstname.setHint("...редактировать: ${userObject.firstname.toString()}")
+        textSecondname.setHint("...редактировать: ${userObject.secondname.toString()}")
+        textAge.setHint("...редактировать: ${userObject.age.toString()}")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putParcelable(KEY_OUTPUT_VALUE, user_object)
+        outState.putParcelable(KEY_OUTPUT_VALUE, userObject)
     }
 
     private fun saveUser() {
-        if (text_firstname.text.trim().isEmpty())
-            text_firstname.error = "ваше имя?"
-        else if (text_secondname.text.trim().isEmpty())
-            text_secondname.error = "ваша фамилия?"
-        else if (text_age.text.trim().isEmpty())
-            text_age.error = "ваш возраст?"
+        if (textFirstname.text.trim().isEmpty())
+            textFirstname.error = "ваше имя?"
+        else if (textSecondname.text.trim().isEmpty())
+            textSecondname.error = "ваша фамилия?"
+        else if (textAge.text.trim().isEmpty())
+            textAge.error = "ваш возраст?"
         else {
-            user_object = UserFormData(
-                text_firstname.text.toString().trim(),
-                text_secondname.text.toString().trim(),
-                text_age.text.toString().trim().toInt()
+            userObject = UserFormData(
+                textFirstname.text.toString().trim(),
+                textSecondname.text.toString().trim(),
+                textAge.text.toString().trim().toInt()
             )
-            user_form_result = Intent()
-            user_form_result.putExtra(OUTPUT_VALUE, user_object)
-            setResult(RESULT_OK, user_form_result)
+            userFormresult = Intent()
+            userFormresult.putExtra(OUTPUT_VALUE, userObject)
+            setResult(RESULT_OK, userFormresult)
             finish()
         }
     }
